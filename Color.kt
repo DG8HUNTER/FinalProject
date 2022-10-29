06d352a4887c863fe217c59df277aaee94a5f496
Georgio Fayad <81255977+DG8HUNTER@users.noreply.github.com>
2022-10-29 15:18:32 +0300

    Add files via upload

--- Color.kt
+++ Color.kt
@@ -1,5 +1,6 @@
 package com.example.expensetrackerproject.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
@@ -19,7 +20,7 @@ val redOrange = Color(0xFFFF4500)
 
 val Green = Color(0XFF59C545)
 val blueLink = Color(0xFF2563eb)
-val Darkblue = Color(0xFF086167)
+val Darkblue = Color(0xFF00A3FF)
 val lightGreen=Color(0xFFF4FFF2)
 val lightYellow = Color(0xFfFFFDC1)
 
@@ -27,6 +28,9 @@ val darkYellow = Color(0xFF9E980C)
 val darkViolet =Color(0xFF2932C9)
 val White = Color(0xFFFFFFFF)
 val darkGray = Color(0xFF3A3A3A)
+val blue1 = Color(0xFF00A3FF)
+val blue2 = Color(0xFF00bfff)
 
+//00A3FF
 
 
